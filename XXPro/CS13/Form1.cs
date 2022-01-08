385c6c8584890286388c43113b268b0c156557af
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-09 07:56:28 +0900

    Udemy C#文法1

---
+++ XXPro/CS13/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 0;
+            i += 2;
+            //i = i + 2;
+
+            i -= 1;
+            // i = i - 1;
+
+            int a = 10 % 4; //aは2
+
+            
+        }
+    }
+}
---
+++ XXPro/CS14/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(textBox1.Text);
+            if (value == 10)
+            {
+                MessageBox.Show("10です");
+            }
+            else if(value == 20)
+            {
+                MessageBox.Show("20です");
+            }
+            else
+            {
+                MessageBox.Show("どれでもない");
+            }
+
+            switch (value)
+            {
+                case 10:
+                    MessageBox.Show("10です");
+                    break;
+
+                default:
+                    MessageBox.Show("どれでもない");
+                    break;
+
+            }
+
+        }
+    }
+}
