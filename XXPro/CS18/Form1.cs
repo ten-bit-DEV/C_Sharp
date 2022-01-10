587b758a57408b72ebc15de6112a05fe9a929271
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-10 09:22:41 +0900

    Udemy C#文法1

---
+++ XXPro/CS18/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> items = new List<int>();
+            items.Add(10);
+            items.Add(20);
+            items.Add(30);
+            items.Add(40);
+
+            foreach(int item in items)
+            {
+                MessageBox.Show(item.ToString());
+            }
+
+        }
+    }
+}
