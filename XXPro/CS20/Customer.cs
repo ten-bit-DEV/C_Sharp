65993bd5299d450c6fe2ea2b5713f0ba3b158f41
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-12 08:36:51 +0900

    Udemy C#文法1

---
+++ XXPro/CS20/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS20
+{
+    class Customer
+    {
+        public string FirstName;
+        public string LastName;
+
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+    }
+}
---
+++ XXPro/CS20/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "aaa";
+            customer.LastName = "bbb";
+            MessageBox.Show(customer.GetFullName());
+        }
+    }
+}
