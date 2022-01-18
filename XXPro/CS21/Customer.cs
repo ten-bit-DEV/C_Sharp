f0bea65abafc63eabade40cf83dce902a726b1bc
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-18 21:22:35 +0900

    Udemy C#文法

---
+++ XXPro/CS21/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS21
+{
+    class Customer
+    {
+        public string FirstName;
+        public string LastName;
+
+        //コンストラクタ
+        public Customer(string firstname, string lastName)
+        {
+            FirstName = firstname;
+            LastName = lastName;
+        }
+
+        public Customer(string lastName) : this("--", lastName)
+        {
+
+        }
+
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+    }
+}
