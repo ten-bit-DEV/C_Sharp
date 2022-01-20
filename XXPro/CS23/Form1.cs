5bbf79a0550d4f81560c8b3a5a7d85ef815e03f5
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-20 17:35:32 +0900

    Udemy C#文法

---
+++ XXPro/CS23/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CheckData(int value, string value2)
+        {
+            //処理
+        }
+
+        public void CheckData(int value)
+        {
+            //処理
+        }
+
+        public int GetData()
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ XXPro/CS24/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS24
+{
+    public partial class Form1 : Form
+    {
+        private int _value = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetData()
+        {
+
+            //型推論
+            //ローカル変数のみ可
+            var aaa = new List<int>();
+            var value = false;
+        }
+    }
+}
