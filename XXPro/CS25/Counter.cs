7b90b80706be9ec12e5dc36d3688f408256b31b5
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-21 21:06:32 +0900

    Udemy C#文法

---
+++ XXPro/CS25/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS25
+{
+    class Counter
+    {
+        private int _count = 0;
+
+        public Counter(int count) {
+
+            _count = count;
+        }
+
+        public void Call()
+        {
+            _count++;
+        }
+
+        public int GetCount()
+        {
+            return _count;
+        }
+    }
+}
---
+++ XXPro/CS25/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Counter counter = new Counter(10);
+            counter.Call();
+            counter.Call();
+            counter.Call();
+            button1.Text = counter.GetCount().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Counter counter2 = new Counter(3);
+            counter2.Call();
+            this.Text = counter2.GetCount().ToString();
+        }
+    }
+}
