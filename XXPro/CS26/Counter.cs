d3076334d43667fbd1ec1af59b27a0b32b00dc92
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-22 21:29:13 +0900

    Udemy C#文法

---
+++ XXPro/CS26/Counter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS26
+{
+    public class Counter
+    {
+        private int _count = 0;
+        private string unit = "回";
+
+        public Counter(int count)
+        {
+            SetCount(count);
+        }
+
+        public void Call()
+        {
+            _count++;
+        }
+
+        public void SetCount(int value)
+        {
+            if(value >= 100)
+            {
+                _count = 100;
+                return;
+            }
+            _count = value;
+        }
+
+        public void GetCount()
+        {
+            return _count;
+        }
+    }
+}
