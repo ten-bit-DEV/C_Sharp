e011723b4b3e4ab122285882ac4d6a0920f6cfce
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-25 18:41:41 +0900

    Udemy C#文法

---
+++ XXPro/CS28/Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS28
+{
+    public class Database:IDisposable
+    {
+        private SqlDataAdapter _adapter = new SqlDataAdapter();
+
+        public void Dispose()
+        {
+            if (_adapter != null)
+            {
+                _adapter.Dispose();
+            }
+        }
+
+        public void Insert()
+        {
+            //SqlConnection connection = new SqlConnection();
+            //try
+            //{
+            //    connection.Open();
+            //    //処理
+            //}
+            //finally
+            //{
+            //    connection.Dispose();   
+            //}
+
+            using(SqlConnection connection = new SqlConnection())
+            {
+                //処理
+            }
+        }
+    }
+}
