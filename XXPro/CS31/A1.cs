78f2b4202cda2619d84792e71efd0a4469c9bf8c
ten-bit-DEV <kadai.kaikou@gmail.com>
2022-01-29 21:36:37 +0900

    Udemy C#文法

---
+++ XXPro/CS31/A1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS30
+{
+    class A1
+    {
+        private int _privatevalue;
+        protected int Protectedvalue;
+
+        public A1(int value)
+        {
+            _privatevalue = value;
+        }
+
+         public virtual int GetData()
+        {
+            return 100;
+        }
+    }
+}
---
+++ XXPro/CS31/A2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS30
+{
+    class A2:A1
+    {
+        //:base()で親クラスのコンストラクタにとおす
+        public A2(int value):base(value)
+        {
+            base.Protectedvalue = 3;
+        }
+
+        public override int GetData()
+        {
+            var value = base.GetData();
+            value += 3;
+            return value;
+        }
+    }
+}
